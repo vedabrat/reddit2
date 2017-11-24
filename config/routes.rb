9036279c6f72da61454f6a506a9db42f57d195bf
vedabrat <vedabrat>
2017-11-24 16:06:51 -0500

    checkpoint Assignment 24 done

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   resources :topics do
     resources :posts, except: [:index]
   end
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create ]
+  post "users/confirm" => 'users#confirm'
+  post "users/new" => 'users#new'
   get 'about' => 'welcome#about'
 
   get 'welcome/faq'
