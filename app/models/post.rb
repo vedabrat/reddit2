e524b2551b72f8de22dd682f63ef8c3693870487
vedabrat <vedabrat>
2017-11-28 10:28:33 -0500

    assignment complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
 
+  scope :ordered_by_title, -> {order('title DESC')}
+  scope :ordered_by_reverse_created_at, -> {order('created_at ASC')}
+
+
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
