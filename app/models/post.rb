6fc1bb0ec4f157e163febb741e60db2d049688c1
vedabrat <vedabrat>
2017-12-01 10:45:43 -0500

    assignment 30 complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
 
+  after_create :create_vote
+
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
@@ -27,5 +29,10 @@ class Post < ActiveRecord::Base
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
   end
+
+private
+def create_vote
+  user.votes.create(value: 1, post: self)
+end
 
 end
