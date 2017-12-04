c1be2896eeb921e8bea3fbaa3661af7f6ad219ac
vedabrat <vedabrat>
2017-12-04 12:40:46 -0500

    work in [pn progress chap32

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
 
   end
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
   get 'about' => 'welcome#about'
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
+  let(:user) {create(:user)}
 
   it {is_expected.to have_many(:posts)}
   it { is_expected.to validate_presence_of(:name) }
@@ -19,7 +19,7 @@ RSpec.describe User, type: :model do
 
   describe "attributes" do
     it "should have name and email attributes" do
-      expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
+      expect(user).to have_attributes(name: user.name, email: user.email)
     end
 
     it "responds to role" do
@@ -91,6 +91,13 @@ RSpec.describe User, type: :model do
     it "returns the appropriate favorite if it exists" do
       favorite = user.favorites.where(post: @post).create
       expect(user.favorite_for(@post)).to eq(favorite)
+    end
+  end
+  describe ".avatar_url" do
+    let(:known_user) { create(:user, email: "blochead@bloc.io")}
+    it "returns the proper Gravatar url for a known email entity" do
+      expected_gravatar = "http://gravatar.com/avatar/bb6d1172212c180cfbdb7039129d7b03.png?s=48"
+      expect(known_user.avatar_url(48)).to eq(expected_gravatar)
     end
   end
 end
