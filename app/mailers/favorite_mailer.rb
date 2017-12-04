dec45fcc80e2132df7886e09aef9f23f71c3d7e3
vedabrat <vedabrat>
2017-12-03 20:12:13 -0500

    chapter 31 done

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -2,9 +2,9 @@ class FavoriteMailer < ApplicationMailer
   default from: "vedabrat@gmail.com"
   def new_comment(user, post, comment)
 
-    headers["Message-ID"] = "<comments/#{comment.id}@techit.com>"
-    headers["In-Reply-To"] = "<post/#{post.id}@techit.com>"
-    headers["References"] = "<post/#{post.id}@techit.com>"
+    headers["Message-ID"] = "<comments/#{comment.id}@whispering-ridge-21186.com>"
+    headers["In-Reply-To"] = "<post/#{post.id}@whispering-ridge-21186.com>"
+    headers["References"] = "<post/#{post.id}@whispering-ridge-21186.com>"
 
     @user = user
     @post = post
